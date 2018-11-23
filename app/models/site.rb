29ad96197f25a170b9cb08093539a09d3bf971d7
大迫雅仁 <velonica2227@icloud.com>
2018-11-23 15:58:09 +0900

    fix: siteのバグ修正

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,8 +4,4 @@ class Site < ApplicationRecord
   validates :site_name,    presence: true
   validates :root_url,     presence: true
   validates :crawling_url, presence: true
-
-  class << self
-    def find_or_create_site()
-  end
 end
