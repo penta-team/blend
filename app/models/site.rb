21ad95c8fdd8fbc5632833ee8815128ea69641cc
大迫雅仁 <velonica2227@icloud.com>
2018-11-27 00:16:20 +0900

    feat: siteの関連を追加

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,8 @@
 class Site < ApplicationRecord
   belongs_to :category
 
+  has_many :sharings
+
   validates :site_name,    presence: true
   validates :root_url,     presence: true
   validates :crawling_url, presence: true
